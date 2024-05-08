f7fe584a6b7a09e2744e967faa4b8703c5bb5e0e
JSE955 <jordanscottedwards@gmail.com>
2024-05-08 12:08:22 -0400

    Update display

--- hangman.rb
+++ hangman.rb
@@ -2,7 +2,7 @@ file = File.open('dictionary.txt')
 dictionary = File.readlines(file).map(&:chomp).select { |word| word.length.between?(5, 12) }
 secret_word = dictionary.sample
 
-display = Array.new(secret_word.size, nil)
+display = Array.new(secret_word.size, '_')
 p secret_word
 puts "Guess a letter"
 guess = gets.chomp

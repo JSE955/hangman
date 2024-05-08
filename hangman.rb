d9e2cf9709f593428829bd686df1ea848966f49d
JSE955 <jordanscottedwards@gmail.com>
2024-05-08 12:08:01 -0400

    Add logic to allow guess and update display

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,15 @@
 file = File.open('dictionary.txt')
 dictionary = File.readlines(file).map(&:chomp).select { |word| word.length.between?(5, 12) }
 secret_word = dictionary.sample
+
+display = Array.new(secret_word.size, nil)
 p secret_word
+puts "Guess a letter"
+guess = gets.chomp
+
+if secret_word.include?(guess)
+  index_list = (0...secret_word.length).select { |i| secret_word[i] == guess }
+  index_list.each { |i| display[i] = guess }
+end
+
+p display

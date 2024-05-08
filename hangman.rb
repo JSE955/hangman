5c828de02c545f482dbcb56622a70187e5e688ba
JSE955 <jordanscottedwards@gmail.com>
2024-05-08 11:48:08 -0400

    Randomly select the secret word

--- hangman.rb
+++ hangman.rb
@@ -1,2 +1,4 @@
 file = File.open('dictionary.txt')
-dictionary = File.readlines(file).map(&:chomp)
+dictionary = File.readlines(file).map(&:chomp).select { |word| word.length.between?(5, 12) }
+secret_word = dictionary.sample
+p secret_word

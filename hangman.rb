bcb7f53edbcb17f28ce64d7f85c4cd9dd66bedba
JSE955 <jordanscottedwards@gmail.com>
2024-05-08 15:58:23 -0400

    Track incorrect guesses

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,24 @@
 
 
 class Hangman
-  attr_accessor :secret_word, :number_of_guesses, :display
+  attr_accessor :secret_word, :number_of_guesses, :display, :incorrect_letters
 
   def initialize
     @secret_word = generate_secret_word
     @number_of_guesses = 10
     @display = Array.new(@secret_word.size, '_')
+    @incorrect_letters = []
+  end
+
+  def show_display
+    puts 'Secret Code:'
+    puts display.join(' ')
+    puts 'Incorrect Guesses:'
+    if incorrect_letters.empty?
+      puts 'None'
+    else
+      puts incorrect_letters.join(' ')
+    end
   end
 
   private
@@ -27,7 +39,8 @@ if game.secret_word.include?(guess)
   index_list = (0...game.secret_word.length).select { |i| game.secret_word[i] == guess }
   index_list.each { |i| game.display[i] = guess }
 else
-game.number_of_guesses -= 1
+  game.number_of_guesses -= 1
+  game.incorrect_letters.push(guess)
 end
 
-p game.display
+game.show_display
